7732ee95ce66d48355fc1151c1c40c145ec6baf3
Hung372001 <cocoten321@gmail.com>
2024-07-23 18:50:36 +0700

    first commit

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BASE_URL: 'https://cdn-kook.kookminvay.com',
+    BASE_URL: 'https://cdn-kook.kookminvay.com/api/v1',
     SOCKET_SERVER_URL: 'https://cdn-kook.kookminvay.com',
     // BASE_URL: 'http://14.225.220.21:8082/api/v1',
     // SOCKET_SERVER_URL: 'http://localhost:8082'
